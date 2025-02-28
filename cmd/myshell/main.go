874bdd7118ec31c2de74bcc6504590b0f0bfb39d
Bhava Sagar <bhavasagar09@gmail.com>
2025-03-01 00:41:39 +0530

    REPL - read evaluete and print the cmds in a loop

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -8,25 +8,26 @@ import (
 )
 
 func main() {
-	fmt.Fprint(os.Stdout, "$ ")
-	input, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading the input: ", err)
-		os.Exit(1)
-	}
+	for true {
+		fmt.Fprint(os.Stdout, "$ ")
+		input, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error reading the input: ", err)
+			os.Exit(1)
+		}
 
-	os := runtime.GOOS
-	max_cap := len(input) - 1
-	if os == "windows" {
-		max_cap--
-	}
-	cmd := input[:max_cap]
+		os := runtime.GOOS
+		max_cap := len(input) - 1
+		if os == "windows" {
+			max_cap--
+		}
+		cmd := input[:max_cap]
 
-	switch cmd {
-	case "TEST_CMD":
-		fmt.Println("Executing TEST_CMD")
-	default:
-		fmt.Println(cmd + ": command not found")
+		switch cmd {
+		case "TEST_CMD":
+			fmt.Println("Executing TEST_CMD")
+		default:
+			fmt.Println(cmd + ": command not found")
+		}
 	}
-
 }
